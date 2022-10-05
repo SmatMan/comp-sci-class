c047104969d1c81c62d858867a7ed06e2f3e154d
SmatMan <gamingajun10@icloud.com>
2022-10-05 17:15:11 +0000

    remove

--- starting/Program.cs
+++ starting/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// random number guessing game
+using System;
+
+namespace starting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 11);
+            int guess = 0;
+            int count = 0;
+            while (guess != number)
+            {
+                Console.Write("Guess my number: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                count++;
+            }
+            Console.WriteLine($"You guessed it in {count} tries.");
+        }
+    }
+}
+
+
