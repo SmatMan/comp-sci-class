cd400f2d26eed5419688345438069839d1ae56c1
SmatMan <gamingajun10@icloud.com>
2022-11-02 20:39:19 +0000

    add even numbers to 20

---
+++ Nov 1 Even Numbers to 20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            while (i <= 20)
+            {
+                Console.WriteLine(i);
+                i += 2;
+            }
+
+            i=2;
+            do
+            {
+                Console.WriteLine(i);
+                i += 2;
+            } while (i <= 20);
+
+            for (i = 2; i <= 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
