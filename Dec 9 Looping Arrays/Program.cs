c78fc4709d35f687baf147aeb6321020fd168744
SmatMan <gamingajun10@icloud.com>
2022-12-13 15:12:22 +0000

    add looping array program

---
+++ Dec 9 Looping Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumArrayElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[6] { 1, 2, 3, 4, 5, 6 };
+
+            int sum = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];
+            }
+
+            Console.WriteLine("The sum of the array elements is: " + sum);
+        }
+    }
+}
